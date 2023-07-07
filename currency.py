235fa1d569acda330b90fdd305ffa1e29362201c
Brian <129234515+brian-g10@users.noreply.github.com>
2023-07-07 07:23:57 -0700

    Update currency.py

--- currency.py
+++ currency.py
@@ -1,5 +1,5 @@
-rur = int(input('Rubles: '))
-usd_rur = rur*0.013
+gr = int(input('Gr: '))
+usd_gr = gr*0.013
 yuan = int(input('Chinese Yuuan: '))
 usd_yuan = yuan*0.15
-print('US Dollars (Mar 28, 2023): ', usd_rur+usd_yuan)
+print('US Dollars (Mar 28, 2023): ', usd_gr+usd_yuan)
